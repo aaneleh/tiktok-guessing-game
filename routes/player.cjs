b283dfcf3bafd3deccd83f32906c825196310147
aaneleh <helenakurzgon@gmail.com>
2025-08-06 19:48:53 -0300

    feat: sorteia video da rodada e excluir jogo ao sair

--- routes/player.cjs
+++ routes/player.cjs
@@ -24,8 +24,6 @@ router.post('/', async(req, res) => {
     let password = req.body.password
     let id_game
 
-    console.log('code: ', code_game)
-
     //GET ID_GAME FROM CODE_GAME
     connection.query('SELECT id_game FROM game WHERE code = ? AND password = ?', [code_game, password], function (error, results, fields) {
         if (error) {
@@ -56,12 +54,58 @@ router.post('/', async(req, res) => {
 router.patch('/:id/ready', async(req, res) => {
     
     let id_player = req.params.id
+    let id_game
 
     connection.query('UPDATE player SET status = 2 WHERE id_player = ?', [id_player], function (error, results, fields) {
         if (error) {
             console.log(error)
             return res.status(500).json({message: 'Erro atualizando jogador'})
         }
+
+        /* PEGA ID_GAME E SE TODOS JOGADORES ESTÃO PRONTOS SORTEIA UM VÍDEO */
+        connection.query('SELECT id_game FROM player WHERE id_player = ?', [id_player], function (error, results, fields) {
+            if (error) {
+                console.log(error)
+                return res.status(500).json({message: 'Erro selecionando jogo'})
+            }
+            id_game = results[0].id_game
+
+            connection.query('SELECT id_player, status FROM player WHERE id_game = ?', [id_game], function (error, results, fields) {
+                if (error) {
+                    console.log(error)
+                    return res.status(500).json({message: 'Erro selecionando jogadores'})
+                }
+
+                let players = []
+                let videos = []
+                let allReady = true
+                for(let i = 0; i < results.length; i++){
+                    players.push(results[i].id_player)
+                    if(results[i].status != 2) allReady = false
+                }
+
+                if(allReady){
+                    connection.query('SELECT id_video FROM video WHERE id_player = ?', [players[Math.floor(Math.random()*players.length)]], function (error, results, fields) {
+                        if (error) {
+                            console.log(error)
+                            return res.status(500).json({message: 'Erro selecionando videos'})
+                        }
+                        videos = results
+                    })
+
+                    connection.query('UPDATE game SET id_video = ?', [videos[Math.floor(Math.random()*videos.length)]], function (error, results, fields) {
+                        if (error) {
+                            console.log(error)
+                            return res.status(500).json({message: 'Erro atualizando jogo'})
+                        }
+                        return res.status(200)
+                    })
+                }
+
+
+            })
+        })
+
         return res.status(200)
     })
 
@@ -95,19 +139,49 @@ router.patch('/:id/guess', async(req, res) => {
 router.delete('/:id/exit', async(req, res) => {
 
     let id_player = req.params.id
+    let id_game
 
-    connection.query('DELETE FROM player WHERE id_player = ?', [id_player], function (error, results, fields) {
+    connection.query('SELECT id_game FROM player WHERE id_player = ?', [id_player], function (error, results, fields) {
         if (error) {
             console.log(error)
             return res.status(500).json({message: 'Erro excluindo jogador'})
         }
-    })
-    
-    connection.query('DELETE FROM video WHERE id_player = ?', [id_player], function (error, results, fields) {
-        if (error) {
-            console.log(error)
-            return res.status(500).json({message: 'Erro excluindo videos'})
-        }
+        console.log(results)
+        id_game = results[0].id_game
+
+        /* EXCLUI JOGADOR E VÍDEO */
+        connection.query('DELETE FROM player WHERE id_player = ?', [id_player], function (error, results, fields) {
+            if (error) {
+                console.log(error)
+                return res.status(500).json({message: 'Erro excluindo jogador'})
+            }
+        })
+        
+        connection.query('DELETE FROM video WHERE id_player = ?', [id_player], function (error, results, fields) {
+            if (error) {
+                console.log(error)
+                return res.status(500).json({message: 'Erro excluindo videos'})
+            }
+        })
+
+        /* SE NÃO TIVER MAIS NENHUM JOGADOR, EXCLUIR JOGO */
+        connection.query('SELECT id_player FROM game WHERE id_game = ?', [id_game], function (error, results, fields) {
+            if (error) {
+                console.log(error)
+                return res.status(500).json({message: 'Erro selecionando jogadores'})
+            }
+
+            if(results.length == 0){
+                connection.query('DELETE FROM game WHERE id_game = ?', [id_game], function (error, results, fields) {
+                    if (error) {
+                        console.log(error)
+                        return res.status(500).json({message: 'Erro excluindo jogo'})
+                    }
+                })
+            }
+        })
+
+
     })
 
     return res.status(200).json({message: 'Jogador e vídeos excluídos com sucesso'})
